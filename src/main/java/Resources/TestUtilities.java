4d4bdc7c27c6e4fac8424af10a966a18cebbbaae
Swapnaneel Mukherjee <46212993+floyd96@users.noreply.github.com>
2020-09-21 19:58:11 +0530

    Update TestUtilities.java

--- src/main/java/Resources/TestUtilities.java
+++ src/main/java/Resources/TestUtilities.java
@@ -1,5 +1,8 @@
 package Resources;
+import java.io.FileInputStream;
+import java.io.IOException;
 import java.util.List;
+import java.util.Properties;
 import java.util.Random;
 
 
@@ -9,6 +12,15 @@ import io.appium.java_client.android.AndroidDriver;
 import io.appium.java_client.android.AndroidElement;
 
 public class TestUtilities extends BaseDriver {
+	
+	public static String getProperty(String key) throws IOException 
+	{
+		FileInputStream fileInputStream = null;
+		fileInputStream = new FileInputStream(System.getProperty("user.dir")+"\\src\\main\\java\\Global.properties");
+		Properties properties=new Properties();
+		properties.load(fileInputStream);
+		return properties.getProperty(key);
+	}
 	
 	
 	
@@ -28,4 +40,3 @@ public class TestUtilities extends BaseDriver {
 	
 	
 }
-
