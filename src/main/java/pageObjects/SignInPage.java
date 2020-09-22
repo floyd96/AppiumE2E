83efef517b46cb0ddb7747cdbf4783580c6586ca
Swapnaneel Mukherjee <46212993+floyd96@users.noreply.github.com>
2020-09-22 10:00:07 +0530

    Update SignInPage.java

--- src/main/java/pageObjects/SignInPage.java
+++ src/main/java/pageObjects/SignInPage.java
@@ -1,8 +1,6 @@
 package pageObjects;
 
 import org.openqa.selenium.support.PageFactory;
-
-import io.appium.java_client.MobileElement;
 import io.appium.java_client.android.AndroidDriver;
 import io.appium.java_client.android.AndroidElement;
 import io.appium.java_client.pagefactory.AndroidFindBy;
@@ -19,29 +17,29 @@ public class SignInPage {
 	
 	//methods
 	@AndroidFindBy(id="com.amazon.mShop.android.shopping:id/sign_in_button")
-	private MobileElement signInBtn;
-	public  MobileElement getSignInBtn() {
+	private AndroidElement signInBtn;
+	public  AndroidElement getSignInBtn() {
 		return signInBtn;
 	}
 	@AndroidFindBy(id="ap_email_login")
-	private MobileElement enterUserId;
-	public  MobileElement getEnterUserId() {
+	private AndroidElement enterUserId;
+	public  AndroidElement getEnterUserId() {
 		return enterUserId;
 	}
 	@AndroidFindBy(id="continue")
-	private MobileElement continueBtn;
-	public  MobileElement getContinueBtn() {
+	private AndroidElement continueBtn;
+	public  AndroidElement getContinueBtn() {
 		return continueBtn;
 	}
 	@AndroidFindBy(id="ap_password")
-	private MobileElement enterPassword;
-	public  MobileElement getEnterPassword() {
+	private AndroidElement enterPassword;
+	public  AndroidElement getEnterPassword() {
 		return enterPassword;
 	}
 	
 	@AndroidFindBy(id="signInSubmit")
-	private MobileElement logInBtn;
-	public  MobileElement getLogInBtn() {
+	private AndroidElement logInBtn;
+	public  AndroidElement getLogInBtn() {
 		return logInBtn;
 	}
 	
